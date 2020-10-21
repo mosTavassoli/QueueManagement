e4b4444d3c2cd688119cd992d670dc7e302c2440
Mostafa <m.tavassoli.it@gmail.com>
2020-10-22 00:44:59 +0200

    fix: improve the Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,120 +1,114 @@
-import React, { Component } from 'react';
-import API from './api/API';
-import './App.css';
-import {BrowserRouter as Switch, Route} from 'react-router-dom';
-import Nav from './components/nav';
-import Body from './components/body';
-import DisplayScreen from './components/DisplayScreen';
-import OfficerScreen from './components/OfficerScreen';
-import {DISPLAY} from './shared/displayScreen';
+import React, { Component } from "react";
+import API from "./api/API";
+import "./App.css";
+import { Switch, Redirect, Route, withRouter } from "react-router-dom";
+import Nav from "./components/nav";
+import Body from "./components/body";
+import DisplayScreen from "./components/DisplayScreen";
+import OfficerScreen from "./components/OfficerScreen";
+import { DISPLAY } from "./shared/displayScreen";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        inProgress : 0,
-        gotTicket:0,
-        displayList : DISPLAY,
-        ticket:{},
-        ticketList:[],
-        services:[],
-       ticketToCall: 453
-      }
+      inProgress: 0,
+      gotTicket: 0,
+      ticket: {},
+      ticketList: [],
+      services: [],
+      ticketToCall: 453,
+    };
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     //Returns list of services , save them on the state and pass them to the components through props
     API.getServices()
-    .then(
-      (services)=>{
-        this.setState({services:services})}
-      )
-    .catch((err)=>console.log(err));
-
-    
+      .then((services) => {
+        this.setState({ services: services });
+      })
+      .catch((err) => console.log(err));
 
     //Get list of tickets served (as public screen), store in the state
-
 
     //
   }
 
-
   //Request new tocket to service (as customer)
   getTicket = (serviceId) => {
-      //console.log("The request : " + serviceId + ", has been selected");
-      this.setState({inProgress:1});
-      API.getTicket(serviceId)
+    //console.log("The request : " + serviceId + ", has been selected");
+    this.setState({ inProgress: 1 });
+    API.getTicket(serviceId)
       .then((ticket) => {
-        this.setState({ticket:ticket});
-        this.setState({inProgress:0, gotTicket:1});})
-        
+        this.setState({ ticket: ticket });
+        this.setState({ inProgress: 0, gotTicket: 1 });
+      })
+
       .catch((errorObj) => {
         this.handleErrors(errorObj);
       });
-  }
+  };
 
   //return to choosing a new ticket
-  handleReturn =()=>{
-    this.setState({inProgress:0, gotTicket:0});
+  handleReturn = () => {
+    this.setState({ inProgress: 0, gotTicket: 0 });
   };
 
   callTicketAsOfficer = (counterId) => {
-    
     //this.setState({ticketToCall: 9});
 
     API.getTicketToServe(counterId)
-    .then((ticket) => {
-      
-      this.setState({ticketToCall: ticket}); // does this work? maybe ticket.something
-    })
-    .catch((errorObj) => {
-      
-      console.log(errorObj);
-      //this.handleErrors(errorObj);
-    });
-  }
+      .then((ticket) => {
+        this.setState({ ticketToCall: ticket }); // does this work? maybe ticket.something
+      })
+      .catch((errorObj) => {
+        console.log(errorObj);
+        //this.handleErrors(errorObj);
+      });
+  };
 
   servedTicketLists = () => {
     API.getListOfServedTickets()
       .then((ticketLists) => {
-         this.setState({
-          ticketList: ticketLists || [],
-        })
-      }     
-      )
-      .catch((errorObj) => {
         this.setState({
-          err: errorObj.errors[0].msg,
+          ticketList: ticketLists || [],
         });
+      })
+      .catch((errorObj) => {
+        console.log(errorObj);
       });
   };
-  
-  render() { 
-    return ( 
+
+  render() {
+    return (
       <>
-      <Nav/>
-      <Switch>
-        <Route exact path="/">
-        <DisplayScreen ticketList={this.state.ticketList} servedTicketLists={this.servedTicketLists}/>
-        <Body
-        gotTicket={this.state.gotTicket} ticket={this.state.ticket} handleReturn={this.handleReturn}
-        inProgress={this.state.inProgress} services={this.state.services} onClick={this.getTicket} />
-        {/* <Switch>
-          <Route path="/" exact component={Body} />
-          <Route path="/counters" component={CarList}/>
-        </Switch> */}
-      </Route>
-      <Route path="/officer">
-        <OfficerScreen ticketToCall={this.state.ticketToCall} callTicket={this.callTicketAsOfficer}/>
-      </Route>
-      </Switch>
-      
-        
-      
+        <Nav />
+        <Switch>
+          <Route exact path="/home">
+            <DisplayScreen
+              ticketList={this.state.ticketList}
+              servedTicketLists={this.servedTicketLists}
+            />
+            <Body
+              gotTicket={this.state.gotTicket}
+              ticket={this.state.ticket}
+              handleReturn={this.handleReturn}
+              inProgress={this.state.inProgress}
+              services={this.state.services}
+              onClick={this.getTicket}
+            />
+          </Route>
+          <Route path="/officer">
+            <OfficerScreen
+              ticketToCall={this.state.ticketToCall}
+              callTicket={this.callTicketAsOfficer}
+            />
+          </Route>
+          <Redirect from="/" exact to="/home" />
+        </Switch>
       </>
-     );
+    );
   }
 }
- 
-export default App;
+
+export default withRouter(App);
